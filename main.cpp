88e2b691de3a7b48c1f61204fe373918b8dc6a81
shiba <82278771+mohmedayman@users.noreply.github.com>
2023-05-21 15:51:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,7 +7,25 @@
 #include "hydrator.hpp"
 #include "standing.hpp"
 #include "team.hpp"
+struct sortv
+{
+     inline bool operator() (Standing  * s1,Standing * s2){
+    if (s1->getPoints()==s2->getPoints())
+    {
+        if(s1->getGoalDiff()==s2->getGoalDiff()){
+            if(s1->getGoalsScored()==s2->getGoalsScored()){
+                return s1->team.getTitle()<s2->team.getTitle();
+            }
+            return s1->getGoalsScored()>s2->getGoalsScored();
+            
+        }
+        return s1->getGoalDiff()>s2->getGoalDiff();
+    }
+    
 
+    return s1->getPoints()>s2->getPoints();
+}
+};
 int main(int argc, char *argv[])
 {
 
@@ -20,7 +38,7 @@ int main(int argc, char *argv[])
         }
 
         std::vector<Standing *> standings = graph.dfs();
-        /* std::sort(standings.begin(), standings.end()); */
+                 std::sort(standings.begin(), standings.end(),sortv()); 
         std::cout << "Team"
                   << ", "
                   << "Matches"
